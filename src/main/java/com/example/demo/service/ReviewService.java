9d6aff6d89009a8c0d8827e477a95ba73e070501
rahulreddy811 <srahulreddy424@gmail.com>
2025-10-25 12:54:02 +0530

    changes

--- src/main/java/com/example/demo/service/ReviewService.java
+++ src/main/java/com/example/demo/service/ReviewService.java
@@ -2,11 +2,15 @@ package com.example.demo.service;
 
 import com.example.demo.model.reviewmodel;
 import com.example.demo.repository.reviewrepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class ReviewService {
 
+    @Autowired
     reviewrepository reviewrepo;
 
     public Optional<reviewmodel> getbymovie(String moviename){
